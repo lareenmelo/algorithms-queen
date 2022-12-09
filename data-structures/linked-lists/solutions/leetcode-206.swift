10b4aeba2b229f46d702d48a75dc154b3de34462
lareenmelo <lareenmmelo@gmail.com>
2022-12-08 21:17:08 -0400

    adding optimal solution for leetcode 206 ☑️

--- data-structures/linked-lists/solutions/leetcode-206.swift
+++ data-structures/linked-lists/solutions/leetcode-206.swift
@@ -1,4 +1,22 @@
 // 206 - Reverse Linked List
+
+// MARK: optimal solution
+func reverseList(_ head: ListNode?) -> ListNode? {
+	var current = head
+	var previous: ListNode?
+
+	while current != nil {
+		let temp = current?.next
+		current?.next = previous
+		previous = current
+		current = temp
+	}
+
+
+	return previous
+}
+
+// MARK: brute force 🤭
 func reverseList(_ head: ListNode?) -> ListNode? {
 	var newList: ListNode?
 	var tempHead = head
