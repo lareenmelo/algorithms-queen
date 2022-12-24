8367ee7538b6cb14a1b11fda156073c7e11e1bdf
lareenmelo <lareenmmelo@gmail.com>
2022-12-24 16:33:41 -0400

    adding solution for leetcode 1832 ☑️

---
+++ data-structures/hash-tables/solutions/leetcode-1832.swift
@@ -0,0 +1,23 @@
+// 1832 - Check if the Sentence Is Pangram
+let alphabet: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+func checkIfPangram(_ sentence: String) -> Bool {
+	var isPangram = true
+	var sentenceLetters = Array(sentence)
+
+	var dictionary: [Character: Int] = [Character: Int]()
+	// creating frequence table of each letter of the sentence
+	for letter in sentenceLetters {
+		dictionary[letter] = 1
+	}
+
+	// checking if all letters of the alphabet are present in
+	// frequency table
+	for letter in alphabet {
+		if dictionary[letter] == nil {
+			return false
+		}
+	}
+
+	return isPangram
+}
